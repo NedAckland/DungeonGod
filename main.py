b55acfad071c3f2dea6f9c0f48ddb68b6a48a24a
NedAckland <48385424+NedAckland@users.noreply.github.com>
2020-01-09 13:53:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
-
+import json
 from discord.ext import commands
 
 # client = discord.Client()
-TOKEN = 'NjY0MjU2ODY4NTQ1NDYyMzAz.XhUd2w.Nl_cjts2BNRmbtTm3-1AUGBXx1U'
+TOKEN = 'NjY0MjU2ODY4NTQ1NDYyMzAz.XhZ9IA.BFnt5k_a3LhSatFtVWvnN3ZptTA'
 
 client = commands.Bot(command_prefix='-')
 
@@ -14,7 +14,6 @@ client = commands.Bot(command_prefix='-')
 # lastly choose a class, options are ect
 # fighting will be turn based story scenarios
 # each race will have a special skill
-
 
 
 class playerCharacter:
@@ -37,55 +36,68 @@ class playerClass:
         self.craft = craft
         self.magic = magic
 
-    def is_warrior(self, player_input):
-        self.attack = 10
-        self.defense = 5
-        self.craft = 0
-        self.magic = 0
+    name = 'un-named please choose a name'
+    class_is = 'no class'
+    race = 'no race'
 
-    def is_thief(self):
-        self.attack = 0
-        self.defense = 5
-        self.craft = 10
-        self.magic = 0
-
-    def set_class(self, player_input):
-        if player_input == 'warrior':
-            self.is_warrior(self)
-        elif player_input == 'thief':
-            self.is_thief()
-        else:
-            print("invalid input")
-
-
-def set_name(message):
-    pass
-
-
-def print_stats(player_input):
-    if player_input == "stats":
-        stats = playerCharacter
-        return stats
+    data = {
+        'name': name,
+        'class': class_is,
+        'race': race
+    }
+    warrior = {
+        'attack': 10,
+        'defense': 5,
+        'craft': 0,
+        'magic': 0
+    }
+    thief = {
+        'attack': 0,
+        'defense': 0,
+        'craft': 10,
+        'magic': 5
+    }
 
 
 @client.event
 async def on_message(message):
     channel = message.channel
-    if message.content.startswith('-class'):
+    mc = message.content
+    pc = playerClass
+    if mc.startswith('-class'):
         await channel.send('choose class:\nwarrior\nthief')
 
         def check(m):
             return m.content == 'warrior' or \
                    m.content == 'thief' and m.channel == channel
-        msg = await client.wait_for('message', check=check)
 
+        msg = await client.wait_for('message', check=check)
         await channel.send('chosen {.content} class!'.format(msg))
-    if message.content.startswith('-name'):
+        a = msg.content
+        pc.data['class'] = a
+        print(pc.data)
+    if mc.startswith('-name'):
         await channel.send('choose name:')
         msg = await client.wait_for('message')
         await channel.send('hello {.content}!'.format(msg))
+        a = msg.content
+        pc.data['id'] = msg.author.id
+        pc.data['name'] = a
+        print(pc.data)
 
-
+    if mc.startswith('-get class'):
+        await channel.send(pc.data.get('class'))
+    if mc.startswith('-get race'):
+        await channel.send(pc.data.get('race'))
+    if mc.startswith('-get name'):
+        await channel.send(pc.data.get('name'))
+    if mc.startswith('-stats'):
+        if pc.data.get('class') == 'warrior':
+            await channel.send(pc.warrior)
+        if pc.data.get('class') == 'thief':
+            await channel.send(pc.thief)
+        if pc.data.get('class') == 'no class':
+            await channel.send("need to pick a class to get stats")
 
 @client.event
 async def on_ready():
